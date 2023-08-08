56ad55244ada25dc93381b043d9c1299911119b6
power <x.oybek94@gmail.com>
2023-08-08 15:29:44 +0900

    Initial commit

---
+++ src/main/kotlin/basics/01_HelloWorld.kt
@@ -0,0 +1,10 @@
+package basics
+
+//An entry point of a Kotlin application is the main function
+fun main(){
+    print("hello world")
+    print("Oybek Kholikov")
+    //new line
+    println("Hello World!")
+    println("Hello Oybek")
+}
